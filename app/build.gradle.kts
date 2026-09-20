5b3dc9c2a34b44515646c76bf9cb1f728fe5e18a
Iout-t <ssweatcatlover@gmail.com>
2026-09-20 10:00:58 +0530

    Update versionName to 1.0 and adjust dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,16 @@ android {
         minSdk = 23
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
@@ -20,5 +29,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+
     implementation("com.google.mlkit:text-recognition:16.0.1")
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 }
