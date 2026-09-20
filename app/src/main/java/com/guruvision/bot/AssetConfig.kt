01d9ebbf737425599fd1709ee5e7706d4d760dee
Iout-t <ssweatcatlover@gmail.com>
2026-09-20 18:21:37 +0530

    Simplify asset configuration list
    Refactored asset configuration to use a more concise syntax for asset entries.

--- app/src/main/java/com/guruvision/bot/AssetConfig.kt
+++ app/src/main/java/com/guruvision/bot/AssetConfig.kt
@@ -8,65 +8,19 @@ data class AssetConfig(
 object AssetConfigList {
 
     val assets = listOf(
+        AssetConfig("EUR/USD (OTC)", "EUR/USD OTC"),
+        AssetConfig("GBP/JPY (OTC)", "GBP/JPY OTC"),
+        AssetConfig("EUR/AUD (OTC)", "EUR/AUD OTC"),
+        AssetConfig("USD/JPY (OTC)", "USD/JPY OTC"),
+        AssetConfig("GBP/USD (OTC)", "GBP/USD OTC"),
+        AssetConfig("AUD/USD (OTC)", "AUD/USD OTC"),
 
-        AssetConfig(
-            "EUR/USD (OTC)",
-            "EUR/USD OTC"
-        ),
+        AssetConfig("BTC/USD", "Bitcoin / USD"),
+        AssetConfig("ETH/USD", "Ethereum / USD"),
+        AssetConfig("SOL/USD", "Solana / USD"),
 
-        AssetConfig(
-            "GBP/JPY (OTC)",
-            "GBP/JPY OTC"
-        ),
-
-        AssetConfig(
-            "EUR/AUD (OTC)",
-            "EUR/AUD OTC"
-        ),
-
-        AssetConfig(
-            "USD/JPY (OTC)",
-            "USD/JPY OTC"
-        ),
-
-        AssetConfig(
-            "GBP/USD (OTC)",
-            "GBP/USD OTC"
-        ),
-
-        AssetConfig(
-            "AUD/USD (OTC)",
-            "AUD/USD OTC"
-        ),
-
-        AssetConfig(
-            "BTC/USD",
-            "Bitcoin / USD"
-        ),
-
-        AssetConfig(
-            "ETH/USD",
-            "Ethereum / USD"
-        ),
-
-        AssetConfig(
-            "SOL/USD",
-            "Solana / USD"
-        ),
-
-        AssetConfig(
-            "GOOGL",
-            "Alphabet"
-        ),
-
-        AssetConfig(
-            "MSFT",
-            "Microsoft"
-        ),
-
-        AssetConfig(
-            "META",
-            "Meta"
-        )
+        AssetConfig("GOOGL", "Alphabet"),
+        AssetConfig("MSFT", "Microsoft"),
+        AssetConfig("META", "Meta")
     )
 }
