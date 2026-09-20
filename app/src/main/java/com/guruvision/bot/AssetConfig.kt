f09515c627b8b4a2e3dfeb222d034f0e4559044d
Iout-t <ssweatcatlover@gmail.com>
2026-09-20 17:27:53 +0530

    Add AssetConfig and AssetConfigList for assets

---
+++ app/src/main/java/com/guruvision/bot/AssetConfig.kt
@@ -0,0 +1,72 @@
+package com.guruvision.bot
+
+data class AssetConfig(
+    val symbol: String,
+    val displayName: String
+)
+
+object AssetConfigList {
+
+    val assets = listOf(
+
+        AssetConfig(
+            "EUR/USD (OTC)",
+            "EUR/USD OTC"
+        ),
+
+        AssetConfig(
+            "GBP/JPY (OTC)",
+            "GBP/JPY OTC"
+        ),
+
+        AssetConfig(
+            "EUR/AUD (OTC)",
+            "EUR/AUD OTC"
+        ),
+
+        AssetConfig(
+            "USD/JPY (OTC)",
+            "USD/JPY OTC"
+        ),
+
+        AssetConfig(
+            "GBP/USD (OTC)",
+            "GBP/USD OTC"
+        ),
+
+        AssetConfig(
+            "AUD/USD (OTC)",
+            "AUD/USD OTC"
+        ),
+
+        AssetConfig(
+            "BTC/USD",
+            "Bitcoin / USD"
+        ),
+
+        AssetConfig(
+            "ETH/USD",
+            "Ethereum / USD"
+        ),
+
+        AssetConfig(
+            "SOL/USD",
+            "Solana / USD"
+        ),
+
+        AssetConfig(
+            "GOOGL",
+            "Alphabet"
+        ),
+
+        AssetConfig(
+            "MSFT",
+            "Microsoft"
+        ),
+
+        AssetConfig(
+            "META",
+            "Meta"
+        )
+    )
+}
