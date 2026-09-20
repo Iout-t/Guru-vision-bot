edd6821bbab411d9af18153b9b174d360b97a73f
Iout-t <ssweatcatlover@gmail.com>
2026-09-20 14:48:32 +0530

    Delete app/src/main/java/com/guruvision/bot/db/AppDatabase.kt

--- app/src/main/java/com/guruvision/bot/db/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-
-package com.guruvision.bot.db
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities=[SignalRecord::class], version=1, exportSchema=false)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun signalDao(): SignalDao
-}
