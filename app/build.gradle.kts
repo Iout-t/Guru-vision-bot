b3953a6df54e9d4839192519e7194e17d286169b
Iout-t <ssweatcatlover@gmail.com>
2026-09-20 11:16:40 +0530

    Update version code and name, add KSP plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -11,17 +13,8 @@ android {
         applicationId = "com.guruvision.bot"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
+        versionCode = 2
+        versionName = "2.0.0"
     }
 }
 
@@ -29,8 +22,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("com.google.mlkit:text-recognition:16.0.1")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("com.google.mlkit:text-recognition:16.0.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 }
